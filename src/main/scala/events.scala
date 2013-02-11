0727b6ff2d296cc2c92337c13b43fb9e5a547a57
softprops <d.tangren@gmail.com>
2013-02-11 08:35:12 -0500

    codify repo events

---
+++ src/main/scala/events.scala
@@ -0,0 +1,19 @@
+package hubcat
+
+object RepoEvents {
+  val Push = "push"
+  val Issues = "issues"
+  val IssueComment = "issue_comment"
+  val CommentComment = "commit_comment"
+  val PullRequest = "pull_request"
+  val PullRequestReviewComment = "pull_request_review_comment"
+  val Gollum = "gollum"
+  val Watch = "watch"
+  val Download = "download"
+  val Fork = "fork"
+  val ForkApply = "fork_apply"
+  val Member = "member"
+  val Public = "public"
+  val TeamAdd = "team_add"
+  val Status = "status"
+}
